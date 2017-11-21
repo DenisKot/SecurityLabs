0d23f6f83b5b7c9db01b1aad722ae0fdbd57cc43
d.kotenko <d.kotenko@elgnc.com>
2017-11-21 21:11:38 +0200

    Adde new lab

---
+++ Modeule2_Lab1/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace Modeule2_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewResult1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxResult1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonCheckout = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.richTextBoxResult2 = new System.Windows.Forms.RichTextBox();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.richTextBoxResult1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.a1,
+            this.a2,
+            this.a3,
+            this.a4});
+            this.dataGridView.Location = new System.Drawing.Point(18, 19);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView.Size = new System.Drawing.Size(332, 45);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(18, 70);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(332, 36);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Зформувати пакет";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // a1
+            // 
+            this.a1.HeaderText = "a1";
+            this.a1.Name = "a1";
+            this.a1.Width = 40;
+            // 
+            // a2
+            // 
+            this.a2.HeaderText = "a2";
+            this.a2.Name = "a2";
+            this.a2.Width = 40;
+            // 
+            // a3
+            // 
+            this.a3.HeaderText = "a3";
+            this.a3.Name = "a3";
+            this.a3.Width = 40;
+            // 
+            // a4
+            // 
+            this.a4.HeaderText = "a4";
+            this.a4.Name = "a4";
+            this.a4.Width = 40;
+            // 
+            // dataGridViewResult1
+            // 
+            this.dataGridViewResult1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResult1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.k1,
+            this.k2,
+            this.k3});
+            this.dataGridViewResult1.Location = new System.Drawing.Point(18, 112);
+            this.dataGridViewResult1.Name = "dataGridViewResult1";
+            this.dataGridViewResult1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridViewResult1.Size = new System.Drawing.Size(332, 45);
+            this.dataGridViewResult1.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "a1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "a2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "a3";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "a4";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 40;
+            // 
+            // k1
+            // 
+            this.k1.FillWeight = 40F;
+            this.k1.HeaderText = "k1";
+            this.k1.Name = "k1";
+            this.k1.Width = 40;
+            // 
+            // k2
+            // 
+            this.k2.FillWeight = 40F;
+            this.k2.HeaderText = "k2";
+            this.k2.Name = "k2";
+            this.k2.Width = 40;
+            // 
+            // k3
+            // 
+            this.k3.FillWeight = 40F;
+            this.k3.HeaderText = "k3";
+            this.k3.Name = "k3";
+            this.k3.Width = 40;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Результат";
+            // 
+            // textBoxResult1
+            // 
+            this.textBoxResult1.Location = new System.Drawing.Point(83, 168);
+            this.textBoxResult1.Name = "textBoxResult1";
+            this.textBoxResult1.Size = new System.Drawing.Size(267, 20);
+            this.textBoxResult1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBoxResult1);
+            this.groupBox1.Controls.Add(this.dataGridView);
+            this.groupBox1.Controls.Add(this.textBoxResult1);
+            this.groupBox1.Controls.Add(this.buttonCalculate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dataGridViewResult1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 305);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Формування пакету";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBoxResult2);
+            this.groupBox2.Controls.Add(this.buttonCheckout);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Location = new System.Drawing.Point(399, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(366, 305);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Перевірка пакету";
+            // 
+            // buttonCheckout
+            // 
+            this.buttonCheckout.Location = new System.Drawing.Point(18, 70);
+            this.buttonCheckout.Name = "buttonCheckout";
+            this.buttonCheckout.Size = new System.Drawing.Size(332, 36);
+            this.buttonCheckout.TabIndex = 1;
+            this.buttonCheckout.Text = "Перевірити пакет";
+            this.buttonCheckout.UseVisualStyleBackColor = true;
+            this.buttonCheckout.Click += new System.EventHandler(this.buttonCheckout_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewTextBoxColumn12,
+            this.dataGridViewTextBoxColumn13,
+            this.dataGridViewTextBoxColumn14,
+            this.dataGridViewTextBoxColumn15});
+            this.dataGridView2.Location = new System.Drawing.Point(18, 19);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView2.Size = new System.Drawing.Size(332, 45);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // richTextBoxResult2
+            // 
+            this.richTextBoxResult2.Location = new System.Drawing.Point(18, 128);
+            this.richTextBoxResult2.Name = "richTextBoxResult2";
+            this.richTextBoxResult2.Size = new System.Drawing.Size(332, 163);
+            this.richTextBoxResult2.TabIndex = 5;
+            this.richTextBoxResult2.Text = "";
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.HeaderText = "a1";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.HeaderText = "a2";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.HeaderText = "a3";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn12
+            // 
+            this.dataGridViewTextBoxColumn12.HeaderText = "a4";
+            this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            this.dataGridViewTextBoxColumn12.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn13
+            // 
+            this.dataGridViewTextBoxColumn13.FillWeight = 40F;
+            this.dataGridViewTextBoxColumn13.HeaderText = "a5";
+            this.dataGridViewTextBoxColumn13.Name = "dataGridViewTextBoxColumn13";
+            this.dataGridViewTextBoxColumn13.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn14
+            // 
+            this.dataGridViewTextBoxColumn14.FillWeight = 40F;
+            this.dataGridViewTextBoxColumn14.HeaderText = "a6";
+            this.dataGridViewTextBoxColumn14.Name = "dataGridViewTextBoxColumn14";
+            this.dataGridViewTextBoxColumn14.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn15
+            // 
+            this.dataGridViewTextBoxColumn15.FillWeight = 40F;
+            this.dataGridViewTextBoxColumn15.HeaderText = "a7";
+            this.dataGridViewTextBoxColumn15.Name = "dataGridViewTextBoxColumn15";
+            this.dataGridViewTextBoxColumn15.Width = 40;
+            // 
+            // richTextBoxResult1
+            // 
+            this.richTextBoxResult1.Location = new System.Drawing.Point(18, 194);
+            this.richTextBoxResult1.Name = "richTextBoxResult1";
+            this.richTextBoxResult1.Size = new System.Drawing.Size(332, 97);
+            this.richTextBoxResult1.TabIndex = 6;
+            this.richTextBoxResult1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 329);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Lab6 © Котенко Денис №415 ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a4;
+        private System.Windows.Forms.DataGridView dataGridViewResult1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxResult1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBoxResult2;
+        private System.Windows.Forms.Button buttonCheckout;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn15;
+        private System.Windows.Forms.RichTextBox richTextBoxResult1;
+    }
+}
+
---
+++ Modeule2_Lab1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modeule2_Lab1
+{
+    using System.Text.RegularExpressions;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            var arr = new List<int>();
+            arr.Add(Convert.ToInt32(dataGridView.Rows[0].Cells[0].Value));
+            arr.Add(Convert.ToInt32(dataGridView.Rows[0].Cells[1].Value));
+            arr.Add(Convert.ToInt32(dataGridView.Rows[0].Cells[2].Value));
+            arr.Add(Convert.ToInt32(dataGridView.Rows[0].Cells[3].Value));
+
+            var stringBuilder = new StringBuilder();
+            //sb.Append("Результат:\r\n");
+            //sb.Append("a1|a2|a3|a4|k1|k2|a3\r\n");
+
+            var k1 = (arr[1] + arr[2] + arr[3]) % 2;
+            var k2 = (arr[0] + arr[2] + arr[3]) % 2;
+            var k3 = (arr[0] + arr[1] + arr[3]) % 2;
+            stringBuilder.Append(string.Join("", arr));
+            stringBuilder.Append(k1);
+            stringBuilder.Append(k2);
+            stringBuilder.Append(k3);
+            var res = stringBuilder.ToString();
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                this.dataGridViewResult1.Rows[0].Cells[i].Value = res[i];
+            }
+            
+            this.textBoxResult1.Text = stringBuilder.ToString();
+
+            ////
+            var sb = new StringBuilder();
+            sb.Append("k1 = a2 + a3 + a4 (MOD 2)\r\n");
+            sb.Append("k2 = a1 + a3 + a4 (MOD 2)\r\n");
+            sb.Append("k3 = a1 + a2 + a4 (MOD 2)\r\n");
+
+            sb.Append($"k1 = {arr[1]} + {arr[2]} + {arr[3]} = " + k1 + "\r\n");
+            sb.Append($"k2 = {arr[0]} + {arr[2]} + {arr[3]} = " + k2 + "\r\n");
+            sb.Append($"k3 = {arr[0]} + {arr[1]} + {arr[3]} = " + k3 + "\r\n");
+
+            this.richTextBoxResult1.Text = sb.ToString();
+        }
+
+        private void buttonCheckout_Click(object sender, EventArgs e)
+        {
+            var arr = new List<int>();
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[0].Value));
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[1].Value));
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[2].Value));
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[3].Value));
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[4].Value));
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[5].Value));
+            arr.Add(Convert.ToInt32(dataGridView2.Rows[0].Cells[6].Value));
+
+            var sb = new StringBuilder();
+            sb.Append("S1 = a4 + a5 + a6 + a7 (MOD 2)\r\n");
+            sb.Append("S2 = a2 + a3 + a6 + a7 (MOD 2)\r\n");
+            sb.Append("S3 = a1 + a3 + a5 + a7 (MOD 2)\r\n");
+
+            var s1 = GetNum(arr, 4, 5, 6, 7);
+            var s2 = GetNum(arr, 2, 3, 6, 7);
+            var s3 = GetNum(arr, 1, 3, 5, 7);
+
+            sb.Append($"S1 = {arr[3]} + {arr[4]} + {arr[5]} + {arr[6]} = " + s1 + "\r\n");
+            sb.Append($"S2 = {arr[1]} + {arr[2]} + {arr[5]} + {arr[6]} = " + s2 + "\r\n");
+            sb.Append($"S3 = {arr[0]} + {arr[2]} + {arr[4]} + {arr[6]} = " + s3 + "\r\n");
+
+            var s = ConvertToInt($"{s1}{s2}{s3}");
+            sb.Append($"S3 = {s1}{s2}{s3} = {s}\r\n");
+            sb.Append(s == 0 ? "Пакет цілісний!" : "В пакеті помилка. Елемент: а" + s);
+
+            richTextBoxResult2.Text = sb.ToString();
+        }
+
+        private int GetNum(IList<int> arr, params int[] p)
+        {
+            var res = 0;
+            foreach (var item in p)
+            {
+                res += arr[item - 1];
+            }
+
+            return res % 2;
+        }
+
+        static readonly Regex binary = new Regex("^[01]{1,32}$", RegexOptions.Compiled);
+        private int ConvertToInt(string s)
+        {
+            if (binary.IsMatch(s))
+            {
+                return Convert.ToInt32(s, 2);
+            }
+            else
+            {
+                throw  new ArgumentException();
+            }
+        }
+    }
+}
